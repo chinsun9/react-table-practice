9fb627bb079c680d89ca5d0e4b868a10bfb81298
chinsun9 <chin_sung@naver.com>
2021-05-06 16:15:52 +0900

    feat Column Ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import ColumnOrder from './components/ColumnOrder';
 // import FilteringTable from './components/FilteringTable';
 // import PaginationTable from './components/PaginationTable';
-import RowSelection from './components/RowSelection';
+// import RowSelection from './components/RowSelection';
 // import BasicTable from './components/BasicTable';
 // import SortingTable from './components/SortingTable';
 
 function App() {
-  return <RowSelection />;
+  return <ColumnOrder />;
 }
 
 export default App;
