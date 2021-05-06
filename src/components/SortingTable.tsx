85cc7d2dea25149d0ee695e426bd4a3baa733a6a
chinsun9 <chin_sung@naver.com>
2021-05-06 10:49:52 +0900

    feat sorting

---
+++ src/components/SortingTable.tsx
@@ -0,0 +1,78 @@
+import './table.css';
+import React, { useMemo } from 'react';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './columns';
+import { useTable, useSortBy } from 'react-table';
+
+export default function SortingTable() {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      // @ts-ignore
+      columns,
+      data,
+    },
+    useSortBy,
+  );
+
+  return (
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column: any) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterGroupProps}>
+                  {column.render('Footer')}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  );
+}
