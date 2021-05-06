5084e32504238339771c7e46091d96ebf3aa50e5
chinsun9 <chin_sung@naver.com>
2021-05-06 13:32:49 +0900

    feat Pagination goto page

--- src/components/PaginationTable.tsx
+++ src/components/PaginationTable.tsx
@@ -20,12 +20,17 @@ export default function PaginationTable() {
     canPreviousPage,
     pageOptions,
     state,
+    gotoPage,
+    pageCount,
     prepareRow,
   } = useTable(
     {
       // @ts-ignore
       columns,
       data,
+      initialState: {
+        pageIndex: 2,
+      },
     },
     usePagination,
   );
@@ -81,11 +86,31 @@ export default function PaginationTable() {
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previous
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
         </button>
       </div>
     </>
