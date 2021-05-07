1d60bfaed5f3a6032ae46ec0c5ef82c906f8b382
chinsun9 <chin_sung@naver.com>
2021-05-07 11:19:34 +0900

    set spa, favicon

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
